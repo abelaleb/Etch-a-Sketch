c17172e4d0ec8aa72033bb26a3a1f27d9b9126e2
abel <abelalebachewasfaw.gmail.com>
2024-03-08 20:46:33 +0300

    Add color and and div to the page

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+const button = document.getElementById('button');
+const inputButton = document.createElement('button');
 const gridContainer = document.getElementById('grid-container');
+inputButton.textContent = "RESET";
+inputButton.setAttribute('id','inputButton')
+inputButton.addEventListener('click', function(){
+    location.reload();
+})
 function handleMouseOver(event) {
     if(event.ctrlKey){
     event.target.style.backgroundColor = '#65524d';
     } 
 }
+
 // Loop to create 16x16 grid of square divs
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
@@ -13,3 +21,4 @@ for (let i = 0; i < 16; i++) {
         gridContainer.appendChild(div);
     }
 }
+button.append(inputButton);
