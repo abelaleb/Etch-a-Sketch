1d05bb2efd8949359c078da48e38f43383387313
abel <abelalebachewasfaw.gmail.com>
2024-03-10 01:53:52 +0300

    Add colors and styles to the css

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
-const button = document.getElementById('button');
-const inputButton = document.createElement('button');
+const button = document.getElementById('resetbtn');
+const resetButton = document.createElement('button');
+const enterButton = document.getElementById('enterButton')
+const gridSizeInput = document.getElementById('gridSizeInput');
 const gridContainer = document.getElementById('grid-container');
-inputButton.textContent = "RESET";
-inputButton.setAttribute('id','inputButton')
-inputButton.addEventListener('click', function(){
+resetButton.textContent = "RESET";
+resetButton.setAttribute('id','resetButton');
+
+resetButton.addEventListener('click', function(){
     location.reload();
-})
+});
+
 function handleMouseOver(event) {
     if(event.ctrlKey){
     event.target.style.backgroundColor = '#65524d';
     } 
 }
 
-// Loop to create 16x16 grid of square divs
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const div = document.createElement('div');
-        div.classList.add('grid-item'); // Add a class for styling
-        div.addEventListener('mouseover', handleMouseOver); // Add mouseover event listener
-        gridContainer.appendChild(div);
+function createGrid(size){
+    gridContainer.innerHTML='';//clear the exeisting grid
+    const gridSize = Math.min(size,100);
+    for (let i=0; i<gridSize; i++){
+        for(let j=0; j<gridSize; j++){
+            const div = document.createElement('div');
+            div.classList.add('grid-item');//Add a class for styling
+            div.addEventListener('mouseover', handleMouseOver);
+            gridContainer.appendChild(div);
+        }
     }
 }
-button.append(inputButton);
+function resetGrid(){
+    const gridSize = parseInt(gridSizeInput.value);
+    if (!isNaN(gridSize)){
+        createGrid(gridSize);
+    }
+    else{
+        alert(`Please enter a valid number for a grid size.`);
+    }
+}
+enterButton.addEventListener('click', function() {
+    resetGrid();
+});
+
+createGrid(16);
+button.appendChild(resetButton);
+
